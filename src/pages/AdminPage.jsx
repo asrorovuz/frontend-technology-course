b277052271ddf7e2a2977f0581eacb1b54a54cce
Asrorov Sardor <asrorovsardor07@gmail.com>
2023-04-23 01:41:26 +0500

    add accardion in docPage

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,48 +1,47 @@
-import React, { useState } from 'react'
-import { Link } from "react-router-dom"
-import { ADoc } from "../components/adminComponent/ADoc"
-import { ACard } from "../components/adminComponent/ACard"
-import { AVideo } from "../components/adminComponent/AVideo"
-import { AUser } from "../components/adminComponent/AUser"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { ADoc } from "../components/adminComponent/ADoc";
+import { ACard } from "../components/adminComponent/ACard";
+import { AVideo } from "../components/adminComponent/AVideo";
+import { AUser } from "../components/adminComponent/AUser";
+import { ReactComponent as LeftLine } from "../assistant/arrow-left.svg";
 
-import "../styles/Admin.css"
+import "../styles/Admin.css";
 
 export const AdminPage = () => {
-  const [renderComp, setRenderComp] = useState(<ADoc />)
+  const [renderComp, setRenderComp] = useState(<ADoc />);
 
   const changeComp = (e) => {
-    let value = e.target.innerText
-    if(value === "Manbalar"){
-      setRenderComp(<ADoc />)
-    }else if(value === "Kurslar"){
-      setRenderComp(<ACard />)
-    }else if(value === "Videolar"){
-      setRenderComp(<AVideo />)
-    }else if(value === "Admin"){
-      setRenderComp(<AUser />)
-    }else{
-      console.log("err")
+    let value = e.target.innerText;
+    if (value === "Manbalar") {
+      setRenderComp(<ADoc />);
+    } else if (value === "Kurslar") {
+      setRenderComp(<ACard />);
+    } else if (value === "Videolar") {
+      setRenderComp(<AVideo />);
+    } else if (value === "Admin") {
+      setRenderComp(<AUser />);
+    } else {
+      console.log("err");
     }
-  }
+  };
 
   return (
-    <div className='admin-page d-flex'>
+    <div className="admin-page d-flex">
       <div className="admin-navbar">
         <h2>
           <Link to="/">
-            Qaytish
+            <LeftLine /> Qaytish
           </Link>
         </h2>
-        <ul className='d-flex' onClick={(e) => changeComp(e)}>
+        <ul className="d-flex" onClick={(e) => changeComp(e)}>
           <li>Manbalar</li>
           <li>Kurslar</li>
           <li>Videolar</li>
           <li>Admin</li>
         </ul>
       </div>
-      <div className="admin-main">
-        {renderComp}
-      </div>
+      <div className="admin-main">{renderComp}</div>
     </div>
-  )
-}
+  );
+};
--- src/pages/DocPage.jsx
+++ src/pages/DocPage.jsx
@@ -3,10 +3,13 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import LoadingComponent from "../components/LoadingComponent";
+import { ReactComponent as UpIcon } from "../assistant/top.svg";
+import { ReactComponent as DownIcon } from "../assistant/bottom.svg";
 import "../styles/Doc.css";
 
 export const DocPage = () => {
   const [data, setData] = useState(null);
+  const [accardion, setAccardion] = useState(null);
 
   const getDoc = () => {
     fetch("https://643a38e6bd3623f1b9af203f.mockapi.io/frontend-tech/document")
@@ -14,27 +17,55 @@ export const DocPage = () => {
       .then(setData);
   };
 
+  const onChangeActive = (id) => {
+    // setChangeIcon(!changeIcon);
+    setAccardion(id);
+  };
+
   useEffect(() => {
     getDoc();
   }, [data]);
 
-  let arr = data?.map((item) => {
+  let arr = data?.map((item, index) => {
     let { title, content, url } = item;
 
+    let id = index + 1
     return (
       <div className="doc-card">
-        <div className="doc-header">
+        <div className="doc-header d-flex">
           <h3>{title}</h3>
+          <div className="icon-doc" onClick={() => onChangeActive(id)}>
+            {id === accardion ? <UpIcon /> : <DownIcon />}
+          </div>
         </div>
-        <div className="doc-main">
+        <div className={id ===  accardion ? "doc-main show" : "doc-main"}>
           <div className="card-content">{content}</div>
           <div className="doc-link">
-            <Link to={url}>show more</Link>
+            <Link to={url}>
+              show more
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="#2b3844"
+                class="bi bi-arrow-right"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+                />
+              </svg>
+            </Link>
           </div>
         </div>
       </div>
     );
   });
 
-  return data ? <div className="container d-flex doc">{arr}</div> : <LoadingComponent/>;
+  return data ? (
+    <div className="container d-flex doc">{arr}</div>
+  ) : (
+    <LoadingComponent />
+  );
 };
